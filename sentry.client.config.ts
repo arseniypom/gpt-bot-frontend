6ff92fdcc5de65eff5441a0e9959f44be2f66bb2
arseniypom <arpomazkov@bk.ru>
2024-10-26 09:11:44 +0300

    Added Sentry

---
+++ sentry.client.config.ts
@@ -0,0 +1,12 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://54ea232fdc95f40c8ea4880e024180d8@o4508179630260224.ingest.de.sentry.io/4508179644809296",
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
